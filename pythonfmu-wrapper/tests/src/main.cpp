ae3cadcae475e6b95c879085f0019e161f0fe922
Christian Legaard <clegaard@outlook.com>
2019-11-26 16:43:51 +0100

    improved test framework

--- pythonfmu-wrapper/tests/src/main.cpp
+++ pythonfmu-wrapper/tests/src/main.cpp
@@ -1,21 +1,8 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-
-TEST_CASE("Testing stuff")
-{
-    REQUIRE(10 == 10);
-}
-
-TEST_CASE("TEsting other stuff")
-{
-    REQUIRE(20 == 10);
-}
-
-/* 
-#include <stdio.h>
+#include "pythonfmu/PyObjectWrapper.hpp"
+// #include <filesystem>
 
 int main(int argc, char **argv)
 {
-    printf("Hello world");
+
     return 0;
-} */
+}
