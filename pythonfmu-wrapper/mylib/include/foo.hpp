43ec1aad78e902bb76abc73067bf3d8ca7101480
Christian <clegaard@outlook.com>
2019-11-30 09:09:21 -0800

    added catch2 test framework

--- pythonfmu-wrapper/mylib/include/foo.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef FOO_HPP
-#define FOO_HPP
-
-void foo();
-
-#endif
--- pythonfmu-wrapper/tests/src/tests.cpp
+++ pythonfmu-wrapper/tests/src/tests.cpp
@@ -1,50 +1,40 @@
-#include <string>
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <filesystem>
 
 #include "pythonfmu/PyObjectWrapper.hpp"
 #include "pythonfmu/PyInitializer.hpp"
-#include "foo.hpp"
 
-#include <string>
-#include <filesystem>
-
-using namespace pythonfmu;
 using namespace std;
 using namespace filesystem;
+using namespace pythonfmu;
 
-int main(int argc, char **argv)
+
+TEST_CASE("PyObjectWrapper")
 {
+    SECTION("FMUS")
+    {
+        path fmu_resource_path("resources/adder/");
+        auto state = PyInitializer();
 
-    // const wchar_t *python_path = L"C:\\ProgramData\\Miniconda3\\Lib";
-    
-    
+        SECTION("Adder")
+        {
+            auto wrapped = PyObjectWrapper(fmu_resource_path.string());
+            
+            unsigned int set_refs[] = {0,1};
+            double set_vals[] = {5,10};
+            wrapped.setReal(set_refs,2,set_vals);
 
-    
-    path fmu_resource_path("resources/adder/");
-    
-    auto state = PyInitializer();
+            
 
-        
-    auto wrapped = PyObjectWrapper(fmu_resource_path.string());
+            wrapped.doStep(0,1);
 
-    
-    
+            unsigned int get_refs[] = {2};
+            double get_vals[] = {0};
+            wrapped.getReal(get_refs,1,get_vals);   
 
-    unsigned int set_refs[] = {0,1};
-    double set_vals[] = {5,10};
 
-    wrapped.setReal(set_refs,2,set_vals);
-
-    unsigned int get_refs[] = {2};
-    double get_vals[] = {0};
-
-    wrapped.doStep(0,1);
-    
-    wrapped.getReal(get_refs,1,get_vals);
-
-    
-
-    cout << "Values are: " << get_vals[0] << std::endl;
-
-    return 0;
+            REQUIRE(get_vals[0] == 15);
+        }
+    }
 }
