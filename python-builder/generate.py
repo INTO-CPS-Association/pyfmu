b46ef07e13479b9d7807eb1f4e442f7e2e59bd1f
Christian <clegaard@outlook.com>
2019-11-30 14:28:04 -0800

    continued work on cmd line program

---
+++ python-builder/generate.py
@@ -0,0 +1,72 @@
+"""
+This module contains functionality to the creation of Python FMU projects
+"""
+from os import makedirs
+from os.path import join, curdir, realpath, dirname
+from shutil import copy
+from jinja2 import Template
+
+
+def _create_dirs(project_path: str, exist_ok: bool = True):
+    source_path = join(project_path, "source")
+
+    try:
+        makedirs(project_path, exist_ok=True)
+        makedirs(source_path, exist_ok=True)
+    except OSError:
+        raise Exception(
+            "Project with identical name already exists in that location. Please specify a new name, another directory or remove the old project.")
+    except Exception as e:
+        raise Exception("Failed to create directories for the Python project")
+
+
+def _generate_fmu_template(project_path: str, source_path: str, template_path: str, main_class_name: str):
+
+    r = None
+    with open(template_path, 'r') as f:
+        s = f.read()
+        template = Template(s)
+
+        r = template.render(
+            {
+                "class_name": main_class_name,
+                "description": "",
+                "model_name": main_class_name,
+                "author": ""
+            }
+        )
+
+    python_fmu_path = join(source_path, main_class_name + ".py")
+
+    with open(python_fmu_path, 'w') as f:
+        f.write(r)
+
+def _copy_python_library_to_sources(python_lib_path : str, source_path: str):
+    
+    python_script_path = join(python_lib_path, "fmi2slave.py")
+
+    copy(python_script_path,source_path)
+
+    
+
+
+
+def create_project(project_path: str, main_class_name: str):
+
+    cur_dir = dirname(realpath(__file__))
+
+    template_path = join(cur_dir, "resources",
+                         "templates", "fmu.py.j2")
+
+    source_path = join(project_path, "source")
+
+    python_lib_path = join(cur_dir,"resources","scripts")
+    
+    
+    _create_dirs(project_path)
+
+    _generate_fmu_template(project_path, source_path,
+                           template_path, main_class_name)
+
+
+    _copy_python_library_to_sources(python_lib_path, source_path)
---
+++ python-builder/py2fmu.py
@@ -0,0 +1,98 @@
+import argparse
+
+
+def config_generate_subprogram(subparser: argparse.ArgumentParser) -> None:
+    parser_gen = subparsers.add_parser(
+        'generate', help="Generate new Python projects from scratch or based on existing resources such as Model Descriptions and reference FMUs.")
+
+    file_project_group = parser_gen.add_mutually_exclusive_group(required=True)
+
+    file_project_group.add_argument('--path', '-p', type=str, help="Path to the project that will be generated")
+
+    file_project_group.add_argument(
+        '--file', '-f', type=str, help="Path to either a model description file or a FMU archive")
+
+    parser_gen.add_argument(
+        '--name', '-n', type=str, required=False, help="Name of the project to be generated. If not specified this information will be extracted from either the path or a model description")
+
+def config_export_subprogram(parser: argparse.ArgumentParser) -> None:
+    parser_export = subparsers.add_parser(
+        'export',
+        help="Export Python projects as FMUs",
+    )
+
+    group_bundle = parser_export.add_argument_group('bundle')
+
+    help_bundle_interpreter = """Bundle a Python Interpreter in the FMU. 
+    This will allow standalone execution on platforms where a suitable Python interpreter is not available, at the cost of an increase in archive size.
+    Note that a interpreter is bundled for each supported platform.
+    """
+    group_bundle.add_argument(
+        "-bi", type=bool, default=False, help=help_bundle_interpreter)
+
+    help_bundle_libs = """Bundle current Python environment Libraries in the FMU. 
+    """
+    group_bundle.add_argument(
+        "-bl", type=bool, default=True, help=help_bundle_libs)
+
+    help_prune_libs = """ Decrease size of the included libraries by only including those used by the FMU.
+    Note that this relies on analyzing static analysis of the Python files and may fail to discover all dependencies.
+    """
+    group_bundle.add_argument(
+        "-bp", type=bool, default=False, help=help_prune_libs)
+
+
+def handle_generate(args):
+
+    from generate import create_project
+    from os.path import join, curdir, dirname, basename, normpath
+
+    if(args.file is not None):
+        raise Exception("Currently generation from file is not supported")
+
+    project_path = join(curdir, args.path)
+
+    main_class_name = args.name if args.name is not None else basename(normpath(project_path))
+
+    create_project(project_path, main_class_name)
+
+
+if __name__ == "__main__":
+
+    description_text = """Utility program to facility the development of Functional Mock-up Units (FMUs) using Python code.
+    """
+
+    example_text = '''examples:
+
+these are common commands for various situations:
+
+generating a new Python project from scratch
+    python py2fmu generate -n engine
+
+generating a new Python project based on a model description file or reference FMU.
+    python py2fmu generate -f modelDescription.xml
+    python py2fmu generate -f engine.fmu
+
+exporting a Python project as an FMU
+    python py2fmu export -p engine
+
+re-configuring an existing project
+    python py2fmu configure -p engine --bundle-interpreter
+    '''
+
+    parser = argparse.ArgumentParser(
+        epilog=example_text, formatter_class=argparse.RawDescriptionHelpFormatter, description=description_text)
+
+    subparsers = parser.add_subparsers(dest="subprogram", required=True)
+
+    config_generate_subprogram(subparsers)
+    config_export_subprogram(subparsers)
+
+    args = parser.parse_args()
+
+    if(args.subprogram == "generate"):
+        handle_generate(args)
+    elif(args.subprogram == "export"):
+        raise Exception("Not implemented")
+    else:
+        raise Exception("Not implemented")
