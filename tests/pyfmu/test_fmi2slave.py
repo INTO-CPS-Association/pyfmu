babd9fd20e82780b4327b2cca1ad95fd795faca9
clegaard <clegaard@outlook.com>
2020-02-01 05:44:03 -0800

    Added more tests

--- tests/pyfmu/test_fmi2slave.py
+++ tests/pyfmu/test_fmi2slave.py
@@ -1,5 +1,5 @@
 from pybuilder.libs.pyfmu.fmi2slave import Fmi2Slave
-from pybuilder.libs.pyfmu.fmi2types import Fmi2DataTypes, Fmi2Causality
+from pybuilder.libs.pyfmu.fmi2types import Fmi2DataTypes, Fmi2Causality, Fmi2Variability
 
 class Adder(Fmi2Slave):
     
@@ -9,6 +9,11 @@ class Adder(Fmi2Slave):
         self.register_variable("a", data_type=Fmi2DataTypes.real, causality = Fmi2Causality.input, start=0)
         self.register_variable("b", data_type=Fmi2DataTypes.real, causality = Fmi2Causality.input, start=0)
         self.register_variable("c", data_type=Fmi2DataTypes.real,causality = Fmi2Causality.output)
+
+class Dummy(Fmi2Slave):
+
+    def __init__(self):
+        super().__init__("Dummy")
 
 class Tests_fmi2Slave:
     
@@ -22,3 +27,39 @@ class Tests_fmi2Slave:
 
         assert(a.a == 0)
         assert(a.b == 0)
+
+
+def test_inputsUseStartValue():
+    
+    d = Dummy()
+    
+    start = 10
+    vr = 0
+
+    d.register_variable("a",data_type = Fmi2DataTypes.real,causality=Fmi2Causality.input, start=start,value_reference=vr)
+
+    result = [0]
+
+    d.__get_real__([vr],result)
+    assert(result[0] == start)
+
+def test_parametersUseStartValue():
+    d = Dummy()
+
+    start = 10
+    vr = 0
+
+    d.register_variable("a",data_type=Fmi2DataTypes.real,variability=Fmi2Variability.fixed,causality = Fmi2Causality.parameter,start=start,value_reference=vr)
+
+    result = [0]
+
+    d.__get_real__([vr],result)
+    assert(result[0] == start)
+
+def test_inputsSetOverridesStart():
+
+    d = Dummy()
+
+    start = 10
+    vr = 0
+    d.register_variable("a",data_type)
