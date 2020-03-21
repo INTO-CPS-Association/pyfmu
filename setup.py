7f2082355d5852957c13d925d1085de6de4ef71c
Christian Legaard <clegaard@outlook.com>
2020-03-21 22:07:44 +0100

    added missing dependency on jinja 2

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="pyfmu",
     version="0.0.1",
+    description='A set of tools for developing functional-mockup-units (FMUs) using the full capabilities of Python.',
+    author='INTO-CPS Association',
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    project_urls={
+        'Bug Tracker': 'https://github.com/INTO-CPS-Association/pyfmu/issues',
+        'Documentation': 'https://into-cps-application.readthedocs.io/en/latest/submodules/pyfmu/docs/index.html',
+        'Source Code': 'https://github.com/INTO-CPS-Association/pyfmu'
+    },
 
-    install_requires=[],
-
+    install_requires=[
+        'Jinja2'
+    ],
     extras_require={
 
         "docs": ["Sphinx", "recommonmark", "sphinx_rtd_theme", "sphinx-autoapi"],
@@ -31,4 +39,3 @@ setup(
 
 
 )
-# /usr/local/lib/python3.7/dist-packages/pyfmu/resources
