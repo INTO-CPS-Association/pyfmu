069b8a7a69da5968e2b69ac89c9f9bb9751dd9d7
clegaard <clegaard@outlook.com>
2020-03-21 04:21:28 -0700

    resources now included in install

--- setup.py
+++ setup.py
@@ -7,11 +7,23 @@ setup(
     package_dir={'': 'src'},
 
     install_requires=["numpy", "pandas", "pytest", "Sphinx",
-                      "recommonmark", "sphinx_rtd_theme", "sphinx-autoapi", "conan", "fmpy","pyqtgraph","scipy"],
+                      "recommonmark", "sphinx_rtd_theme", "sphinx-autoapi", "conan", "fmpy", "pyqtgraph", "scipy"],
+
+
+    # resources needed by the CLI to generate and export 
+    package_data={
+        "pyfmu": [
+            "resources/templates/*.j2",
+            "resources/wrapper/binaries/*/*[.so|.dll]",
+            "resources/validation/*"
+        ]
+    },
+    include_package_data=True,
 
     python_requires='>=3',
-    
+
     entry_points={'console_scripts': ['pyfmu=pyfmu.pyfmu_cli:main']}
 
 
 )
+# /usr/local/lib/python3.7/dist-packages/pyfmu/resources
