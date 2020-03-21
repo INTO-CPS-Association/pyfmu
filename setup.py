da33ff088472648840f8b29c36a5e0b4004209e7
Christian Legaard <clegaard@outlook.com>
2020-03-21 17:54:17 +0100

    added extra_requires to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
 
-    install_requires=["numpy", "pandas", "pytest", "Sphinx",
-                      "recommonmark", "sphinx_rtd_theme", "sphinx-autoapi", "conan", "fmpy", "pyqtgraph", "scipy"],
+    install_requires=[],
 
+    extras_require={
 
-    # resources needed by the CLI to generate and export 
+        "docs": ["Sphinx", "recommonmark", "sphinx_rtd_theme", "sphinx-autoapi"],
+        "tests": ["numpy", "pandas", "pytest", "fmpy", "pyqtgraph", "scipy", "PyQt5"],
+        "gui": []
+    },
+
+    # resources needed by the CLI to generate and export
     package_data={
         "pyfmu": [
             "resources/templates/*.j2",
