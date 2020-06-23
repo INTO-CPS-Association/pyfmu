f1891e2042baaa1da7feae6bb14da5b207d353d7
Christian Legaard <clegaard@outlook.com>
2020-06-23 11:08:54 +0200

    Better resolution of example uri's from rust

--- pyfmu_wrapper/src/utils.rs
+++ pyfmu_wrapper/src/utils.rs
@@ -1,18 +1,27 @@
-use std::env::current_dir;
+use pyo3::types::PyModule;
+use pyo3::Python;
 
 pub fn get_example_resources_uri(example_name: &str) -> String {
-    let cd = current_dir().expect("unable to read current working directory");
+    let gil = Python::acquire_gil();
+    let py = gil.python();
 
-    let resources_dir = cd
-        .parent()
+    let utils = PyModule::from_code(
+        py,
+        r#"
+from pathlib import Path
+def get_example_resources_uri(example_name: str) -> str:
+    return Path(
+        Path.cwd().parent / "examples" / "exported" / example_name / "resources"
+    ).as_uri()
+    "#,
+        "utils",
+        "utils.py",
+    )
+    .expect("unable to import module");
+
+    utils
+        .call1("get_example_resources_uri", (example_name,))
         .unwrap()
-        .join("examples")
-        .join("exported")
-        .join(example_name)
-        .join("resources")
-        .as_path()
-        .canonicalize()
-        .unwrap();
-
-    format!("file:///{}", resources_dir.to_str().unwrap())
+        .extract()
+        .unwrap()
 }
